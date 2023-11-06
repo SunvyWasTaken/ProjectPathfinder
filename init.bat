1805b6db6f897714af4a9a4244c270323f226804
DevOfSomewhere <keanu.vongue@gmail.com>
2023-11-06 20:06:44 +0100

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,6 +1,17 @@
 @echo off
 set currentPath=%cd%
 
+ECHO Initializing vcpkg
+ECHO =============================
+cmd /c "cd vcpkg && bootstrap-vcpkg.bat -disableMetrics"
+
+ECHO Initializing vcpkg packages
+ECHO =============================
+cd vcpkg
+vcpkg install sfml
+vcpkg install imgui
+vcpkg install imgui-sfml
+
 ECHO Initializing CMake project
 ECHO =============================
 cd %currentPath%
