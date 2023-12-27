123ae3cd9724855761c99833dcb4742f77bafe33
DevOfSomewhere <keanu.vongue@gmail.com>
2023-12-27 09:45:04 +0100

    Begin input handle

--- Spoon/src/Spoon/Core/InputHandler.cpp
+++ Spoon/src/Spoon/Core/InputHandler.cpp
@@ -2,5 +2,18 @@
 
 void InputHandler::BindInput(void* objRef, std::function<void()>& func)
 {
-	
+	Truc tmpFollower(objRef, func);
+	auto tmp = std::find(ListFollower.begin(), ListFollower.end(), tmpFollower);
+	if (tmp == ListFollower.end())
+		return;
+
+	ListFollower.push_back(tmpFollower);
+}
+
+void InputHandler::MassSuicide()
+{
+	for (auto follower : ListFollower)
+	{
+		follower.first
+	}
 }
--- Spoon/src/Spoon/Core/InputHandler.h
+++ Spoon/src/Spoon/Core/InputHandler.h
@@ -6,8 +6,13 @@
 class SPOON_API InputHandler
 {	
 public:
+	using Truc = std::pair<void*, std::function<void()>&>;
 
 	void BindInput(void* objRef, std::function<void()>& func);
+
+	void MassSuicide();
+
+	std::vector<Truc> ListFollower;
 
 
 
